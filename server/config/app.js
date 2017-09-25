67fb4727eb141963857a8b0c3965c07f3dc94e60
jLouis <jeandlouis1@gmail.com>
2017-09-24 23:26:59 -0400

    alter server connection

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,14 @@
 var config = require('./config'), 
     mongoose = require('mongoose'),   
     express = require('./express');
+    var port = process.env.PORT || 8080;
 
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+  app.listen(port, function() {
+    console.log('App listening on port', port);
   });
 };
